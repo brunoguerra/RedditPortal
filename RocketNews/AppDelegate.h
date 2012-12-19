b1f317250474532100b2108a037905d083cca9a7
Travis Hoover <thoov@Traviss-MacBook-Pro.local>
2012-12-18 23:30:38 -0800

    Initial Commit

---
+++ RocketNews/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RocketNews
+//
+//  Created by Travis Hoover on 12/18/12.
+//  Copyright (c) 2012 Travis Hoover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
